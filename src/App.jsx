2fc2c5b2fb77a64bf67245124cad46d3f714954d
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-04-03 11:46:20 +0200

    version avec score qui s'affiche à la fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,134 +1,142 @@
-import { useState } from 'react'
-import './App.css'
-import CardQuestion from './components/CardQuestion'
+import { useState } from "react";
+import "./App.css";
+import CardQuestion from "./components/CardQuestion";
+import ScoreSection from "./components/ScoreSection";
 
 function App() {
+  const questions = [
+    {
+      numéro: 1,
+      intitulé:
+        "Q1. Pendant leurs cours, les professeurs d’économie parlent souvent du “pouvoir...",
+      a: "A. Des chats",
+      b: "B. D'Aïcha",
+      c: "C. Des geishas",
+      d: "D. D'achat" /* bonne réponse*/,
+      prix: "",
+      br: "D. D'achat",
+    },
 
-  const questions = [{
-    numéro : 1, 
-    intitulé : "Q1. Pendant leurs cours, les professeurs d’économie parlent souvent du “pouvoir...", 
-    a : "A. Des chats",
-    b : "B. D'Aïcha",
-    c : "C. Des geishas",
-    d : "D. D'achat",  /* bonne réponse*/
-    prix : "",
-    br : "D. D'achat"
-  },
+    {
+      numéro: 2,
+      intitulé: "Q2. Quel est le petit du lapin ?",
+      a: "A. Lapinou",
+      b: "B. Lapineau",
+      c: "C. Lapereau" /*bonne réponse*/,
+      d: "D. Laperon",
+      prix: "",
+      br: "C. Lapereau",
+    },
+    {
+      numéro: 3,
+      intitulé: "Q3. Quel est le premier producteur mondial de bananes ?",
+      a: "A. La Chine",
+      b: "B. La Tanzanie",
+      c: "C. Le Brésil",
+      d: "D. L'Inde" /*bonne réponse*/,
+      prix: "",
+      br: "D. L'Inde",
+    },
+    {
+      numéro: 4,
+      intitulé:
+        "Q4. Laquelle de ces chansons n'est pas interprétée par Madonna ?",
+      a: "A. Purple Rain",
+      b: "B. True Blue",
+      c: "C. Holiday",
+      d: "D. Vogue" /*bonne réponse*/,
+      prix: "",
+      br: "A. Purple Rain",
+    },
 
-  {
-    numéro : 2, 
-    intitulé : "Q2. Quel est le petit du lapin ?", 
-    a : "A. Lapinou",
-    b : "B. Lapineau",
-    c : "C. Lapereau",  /*bonne réponse*/ 
-    d : "D. Laperon", 
-    prix : "",
-    br : "C. Lapereau",
-  },
-  {
-    numéro : 3, 
-    intitulé : "Q3. Quel est le premier producteur mondial de bananes ?", 
-    a : "A. La Chine",
-    b : "B. La Tanzanie",
-    c : "C. Le Brésil",
-    d: "D. L'Inde", /*bonne réponse*/
-    prix : "",
-    br : "D. L'Inde",
-  },
-  {
-    numéro : 4, 
-    intitulé : "Q4. Laquelle de ces chansons n'est pas interprétée par Madonna ?", 
-    a : "A. Purple Rain",
-    b : "B. True Blue",
-    c : "C. Holiday",
-    d : "D. Vogue", /*bonne réponse*/ 
-    prix : "",
-    br : "A. Purple Rain"
-  },
+    {
+      numéro: 5,
+      intitulé:
+        "Q5. Quel célèbre couturier a dessiné la couverture du petit Larousse ?",
+      a: "A. Yves Saint Laurent",
+      b: "B. Christian Lacroix" /*bonne réponse */,
+      c: "C. Karl Lagerfield",
+      d: "D. Jean-Paul Gaultier",
+      prix: "",
+      br: "B. Christian Lacroix",
+    },
+    {
+      numéro: 6,
+      intitulé:
+        "Q6. Qu'est-ce que la climacophobie ? La peur incontrôlable ... ",
+      a: "A. Des fôrets",
+      b: "B. De la boue",
+      c: "C. Des escaliers" /*bonne réponse*/,
+      d: "D. De l'encre",
+      prix: "",
+      br: "C. Des escaliers",
+    },
+  ];
 
-  {
-    numéro : 5, 
-    intitulé : "Q5. Quel célèbre couturier a dessiné la couverture du petit Larousse ?", 
-    a : "A. Yves Saint Laurent",
-    b : "B. Christian Lacroix", /*bonne réponse */ 
-    c : "C. Karl Lagerfield",
-    d : "D. Jean-Paul Gaultier",
-    prix : "",
-    br: "B. Christian Lacroix"
+  /* Pour faire défiler les questions avec le boutons suivant */
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const suivant = () => {
+    setQuestionIndex((prevIndex) => prevIndex + 1);
+    setActive(!active);
+  };
 
-  },
-  {
-    numéro : 6, 
-    intitulé : "Q6. Qu'est-ce que la climacophobie ? La peur incontrôlable ... ", 
-    a : "A. Des fôrets",
-    b : "B. De la boue",
-    c : "C. Des escaliers", /*bonne réponse*/ 
-    d : "D. De l'encre",
-    prix : "",
-    br : "C. Des escaliers"
+  /*Pour changer la couleur et indiquer à l'utilistateur la bonne réponse */
+  const [active, setActive] = useState(false);
 
-  }
-]
+  /* Pour calculer le score */
+  const [score, setScore] = useState(0);
+  const fct = (selectedAnswer) => {
+    if (selectedAnswer == questions[questionIndex].br) {
+      setScore(score + 1);
+    }
+  };
 
-/* Pour faire défiler les questions avec le boutons suivant */ 
-const [questionIndex, setQuestionIndex] = useState(0)
-const suivant = () => {setQuestionIndex((prevIndex) => (prevIndex + 1)) ; setActive(!active);           
-};
+  /* Combinaison des deux fonctions */
+  const reponseQuizz = (selectedAnswer) => {
+    setActive(!active); 
+    fct(selectedAnswer); 
+  };
 
-/*Pour changer la couleur et indiquer à l'utilistateur la bonne réponse */ 
-const [active, setActive] = useState(false)
-
-
-/* Pour calculer le score */
-const [score, setScore] = useState(0);
-
-const fct = (selectedAnswer) => {
-  if (selectedAnswer == questions[questionIndex].br) {
-    setScore(score + 1);
-  }
-};
-
-
-/* Combinaison des deux fonctions */ 
-const reponseQuizz = (selectedAnswer) => {
-  setActive(!active); // Active la couleur du bouton
-  fct(selectedAnswer); // Vérifie la réponse et met à jour le score
-};
-
-
-
-
+  /*Afficher les résultats*/
+  const [show, setShow] = useState(false);
+   
   return (
     <>
-    
-   <h1>Qui veut gagner ... des trucs ? </h1>
-   <div className='sectionssite'>
-   <section className='quizz'>
-   <CardQuestion question={questions[questionIndex]} questions={questions} reponseQuizz={reponseQuizz}
+      <h1>Qui veut gagner ... des trucs ? </h1>
+
+      <div className="sectionssite">
+        <section className="quizz">
+          <CardQuestion
+            question={questions[questionIndex]}
+            reponseQuizz={reponseQuizz}
             active={active}
-            setActive={setActive} >
+          ></CardQuestion>
 
-   </CardQuestion>
-   <button className="boutonSuivant" onClick={() => {suivant()}}
-            disabled={questionIndex >= questions.length - 1 ? "true" : ""}>Suivant</button>
-   </section>
+          <div className="buttonsFin">
+            <button
+              className="boutonSuivant"
+              onClick={() => {
+                suivant();
+              }}
+              disabled={questionIndex >= questions.length - 1 ? "true" : ""}
+            >
+              Suivant
+            </button>
 
-   
-   <section className='score'>
- 
-   <figure className='scoring'>
-   <h2>Votre score    </h2>
-   <div className="nombreScore"> {score}</div>
-   <h3 className='bravo'> Félicitations ! <br/> Vous avez gagné une ✨ div ✨ </h3>
-  </figure></section>
-
-
-   </div>
-   </>
-    
-  )
+            <button className="showResults" onClick={() => setShow(!show)}
+              disabled={questionIndex < questions.length - 1 ? "true" : ""}
+            >
+              Résultats{" "}
+            </button>
+          </div>
+          </section>
+          
+         {show ? <ScoreSection score={score}>{score} </ScoreSection> : null}
+          
+       
+      </div>
+    </>
+  );
 }
 
-
-export default App
-
+export default App;
